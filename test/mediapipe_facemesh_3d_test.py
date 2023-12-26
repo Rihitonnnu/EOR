961634a11fd19c8ed641da2958d71a9183077890
Rihitonnnu <hexiliang04@gmail.com>
2023-12-26 13:39:11 +0900

    3D座標で距離を計算

--- test/mediapipe_facemesh_3d_test.py
+++ test/mediapipe_facemesh_3d_test.py
@@ -31,19 +31,6 @@ while cap.isOpened():
                     # ランドマークを描画
                     h, w, c = frame.shape
                     cv2.circle(frame, (int(x0 * w), int(y0 * h)), 1, (0, 0, 255), -1)
-                    # 座標をopencvでウィンドウに表示
-                    # cv2.putText(frame, f'({x0:.3f}, {y0:.3f}, {z0:.3f})', (int(x0 * w), int(y0 * h)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-                    # print(f'Landmark {id}: ({x0}, {y0}, {z0})')
-                
-                # 右目の目尻
-                # elif id == 263:
-                #     x1, y1, z1 = lm.x, lm.y, lm.z
-                #     # ランドマークを描画
-                #     h, w, c = frame.shape
-                #     cv2.circle(frame, (int(x1 * w), int(y1 * h)), 1, (0, 0, 255), -1)
-                #     # 座標をopencvでウィンドウに表示
-                #     # cv2.putText(frame, f'({x1:.3f}, {y1:.3f}, {z1:.3f})', (int(x1 * w), int(y1 * h)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-                #     # print(f'Landmark {id}: ({x1}, {y1}, {z1})')
                 
                 # 左目の目頭
                 elif id == 133:
@@ -51,21 +38,11 @@ while cap.isOpened():
                     # ランドマークを描画
                     h, w, c = frame.shape
                     cv2.circle(frame, (int(x2 * w), int(y2 * h)), 1, (0, 0, 255), -1)
-                    # 座標をopencvでウィンドウに表示
-                    # cv2.putText(frame, f'({x2:.3f}, {y2:.3f}, {z2:.3f})', (int(x2 * w), int(y2 * h)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-                    # print(f'Landmark {id}: ({x2}, {y2}, {z2})')
                 
                 #　x0,y0,z0とx2,y2,z2のユークリッド距離を計算
+                    eor = ((x0 - x2)**2 + (y0 - y2)**2 + (z0 - z2)**2)**(1/2)
+                    print(f'eor: {eor}')
                 
-                # # 右目の目頭
-                # elif id == 362:
-                #     x3, y3, z3 = lm.x, lm.y, lm.z
-                #     # ランドマークを描画
-                #     h, w, c = frame.shape
-                #     # cv2.circle(frame, (int(x3 * w), int(y3 * h)), 1, (0, 0, 255), -1)
-                #     # 座標をopencvでウィンドウに表示
-                #     # cv2.putText(frame, f'({x3:.3f}, {y3:.3f}, {z3:.3f})', (int(x3 * w), int(y3 * h)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-                #     # print(f'Landmark {id}: ({x3}, {y3}, {z3})')
 
     # 画像を表示
     cv2.imshow('MediaPipe FaceMesh', frame)
