23c717f30aaa19d0a07f247c43c4a35b2635fbd8
Rihitonnnu <hexiliang04@gmail.com>
2024-01-08 21:46:08 +0900

    不要なファイル削除

--- udp_send.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-def udp_client(message, server_ip, server_port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(message.encode(), (server_ip, server_port))
-
-
-udp_client("Hello, Server!", '127.0.0.1', 2002)
